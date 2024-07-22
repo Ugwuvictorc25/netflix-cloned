358c995ff0a6694bcc1d9cd57ff712f077798c5b
Ugwu Victor Chika <ugwuvictor2097.com>
2024-07-22 19:08:21 +0100

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -7,23 +7,16 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-// 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-// 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-// 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKECT,
-// 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
-// 	appId: process.env.REACT_APP_FIREBASE_API_ID,
-// };
-
 const firebaseConfig = {
-	apiKey: "AIzaSyAaQGBZglW5KeOKS0F7lWdG_IzzlGyy5cM",
-	authDomain: "netflix-react-yt-786ad.firebaseapp.com",
-	projectId: "netflix-react-yt-786ad",
-	storageBucket: "netflix-react-yt-786ad.appspot.com",
-	messagingSenderId: "636622589314",
-	appId: "1:636622589314:web:2851be11af33095ed3d39b",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKECT,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+	appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
